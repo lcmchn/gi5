a506e56c376d365a0fc84de22929d463fac19a0d
lcm <1187419176@qq.com>
2023-05-02 12:55:34 +0800

    mater-push

---
+++ frontest/src/main/java/com/lcm/Hi.java
@@ -0,0 +1,12 @@
+package com.lcm;
+
+/**
+ * @author: lcm
+ * @since: 2023/5/2 12:54
+ * @description:
+ */
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
